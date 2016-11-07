18a3b99acd987b88521b493e5ace52c6972dfb93
homirun <hatunemiku.moco@gmail.com>
2016-11-07 22:56:24 +0900

    ・上の壁だけずれるバグを修正 理由は壁の配列の要素を消すときに処理が遅延するせいの模様 ・デバッグモード追加 trueにすると壁の当たり判定がなくなる

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,7 @@ void Main()
 {
 	Window::Resize(800, 500);
 	Window::SetTitle(L"Flappy Ball");
-	int fast = 0, flag = 0, flame = 0;
+	int fast = 0, flag = 0, frame = 0;
 	static const Vec2 g(0, 0.5);	//重力加速度
 	static const Font font(23);
 	static const Font scorefont(15);
@@ -27,12 +27,21 @@ void Main()
 	HiScore = Parse<int>(reader.readAll());
 	reader.close();
 
+	/****デバッグモード切り替え(default:false)***/
+	static const bool Debug_Mode=false; 
+	/********************************************/
+
 	while (System::Update())
 	{
 		if (fast == 0) {	//最初の１回だけ行う処理
 			font(L"Flappy Ball").drawCenter({ Window::Width() / 2, Window::Height() / 2 });
 			scorefont(L"Enterでゲームスタート").drawCenter({ Window::Width() / 2, Window::Height() / 2 + 60 });
-			scorefont(L"HiScore", (int)HiScore).drawCenter({Window::Width()/2,Window::Height()/2+80});
+			scorefont(L"HiScore", (int)HiScore).drawCenter({Window::Width()/2,Window::Height()/2+90});
+			if (Debug_Mode == true) {
+				scorefont(L"Debug_Mode...").drawCenter({ Window::Width() / 2,Window::Height() / 2 + 120 });
+
+
+			}
 			if (Input::KeyEnter.clicked) {
 				fast = 1;
 			}
@@ -40,7 +49,7 @@ void Main()
 
 		else if (flag == 0) {
 
-			flame++;
+			frame++;
 			PP += g;
 
 			scorefont(score).draw(5, 5);
@@ -55,37 +64,37 @@ void Main()
 			if (P.y <= 10) {	//上に飛び出そうな時
 				P.y = 10;
 			}
-			if (flame % 120 == 0) {	//壁出現タイミング
+			if (frame % 120 == 0) {	//壁出現タイミング
 				int rand = Random(100, 350);
 				wall.push_back(RectF(wallSize.x, 0, 30, rand));
 				wall.push_back(RectF(wallSize.x, rand + 100, 30, Window::Height() - 100 - rand));
 				checkhit.push_back(RectF(wallSize.x, rand, 30, 100));
 				hitflag.push_back(0);
 			}
-			for (int i = 0; i < wall.size(); i++) {
+			for (unsigned int i = 0; i < wall.size(); i++) {
 				wall[i].draw();
 				wall[i].moveBy(Vec2{ -3, 0 });
 
-				if (P.intersects(wall[i])) {	//当たり判定
+				if (P.intersects(wall[i])&&Debug_Mode==false) {	//当たり判定
 					flag = 1;
 				}
-				if (wall[i].x < -30) {
-					wall.erase(wall.begin() + i);
-				}
+				//if (wall[i].x < -30) {
+					//wall.erase(wall.begin() + i);
+				//}
 			}
-			for (int i = 0; i < checkhit.size(); i++) {	//当たり判定の四角形
+			for (unsigned int i = 0; i < checkhit.size(); i++) {	//当たり判定の四角形
 				checkhit[i].moveBy(Vec2{ -3, 0 });
 				if (P.intersects(checkhit[i]) && hitflag[i] == 0) {
 					score++;
 					hitflag[i] = 1;
 				}
-				if (checkhit[i].x < 0) {
-					checkhit.erase(checkhit.begin() + i);
-					hitflag.erase(hitflag.begin() + i);
-				}
+				//if (checkhit[i].x < 0) {
+					//checkhit.erase(checkhit.begin() + i);
+					//hitflag.erase(hitflag.begin() + i);
+				//}
 			}
-			if (flame >= 12000) {	//オーバーフロー防止
-				flame = 0;
+			if (frame >= 12000) {	//オーバーフロー防止
+				frame = 0;
 			}
 			P.draw(Color(0, 200, 255));
 		}
@@ -106,8 +115,10 @@ void Main()
 					HiScore = score;
 					HiScore_Flag = false;
 				}
-
-				flame = 0;
+				wall.clear();
+				checkhit.clear();
+				hitflag.clear();
+				frame = 0;
 				score = 0;
 				flag = 0;
 				wall.clear();
